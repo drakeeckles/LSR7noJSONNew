c839e19c90fc2880945b57463b84b09846ea2645
Drake Eckles <drake.eckles@lsr7.net>
2017-06-07 19:14:33 +0000

    Changes made by Drake Eckles (drake.eckles@lsr7.net).

--- LSR7 No JSON New/app.js
+++ LSR7 No JSON New/app.js
@@ -201,5 +201,15 @@
 
 // START_CUSTOM_CODE_kendoUiMobileApp
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+app.openLink = function(url) {
+        if (url.substring(0, 4) === 'geo:' && device.platform === 'iOS') {
+            url = 'http://maps.apple.com/?ll=' + url.substring(4, url.length);
+        }
 
+        window.open(url, '_system');
+        if (window.event) {
+            window.event.preventDefault && window.event.preventDefault();
+            window.event.returnValue = false;
+        }
+    };
 // END_CUSTOM_CODE_kendoUiMobileApp
